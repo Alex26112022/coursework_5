4ebe742dd6c5712c5480497a3a9e96f7f8f2825d
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-05-11 20:09:06 +0300

    Добавил функции создания таблиц.

--- src/db_create.py
+++ src/db_create.py
@@ -16,9 +16,54 @@ class DbCreate:
         conn.autocommit = True
         with conn.cursor() as cur:
             try:
-                cur.execute(f'DROP DATABASE {name_new_database}')
                 cur.execute(f'CREATE DATABASE {name_new_database}')
-            except:
-                cur.execute(f'CREATE DATABASE {name_new_database}')
+            except psycopg2.Error:
+                pass
+        conn.commit()
+        conn.close()
+
+    def create_table_companies(self, name_new_database='hh_base',
+                               table_name='companies'):
+        """ Создает таблицу компании. """
+        conn = psycopg2.connect(dbname=name_new_database, **self.conn_params)
+        conn.autocommit = True
+        with conn.cursor() as cur:
+            cur.execute(f"""
+                        CREATE TABLE IF NOT EXISTS {table_name} (
+                        company_id VARCHAR(20) PRIMARY KEY,
+                        company_name VARCHAR(100),
+                        company_city VARCHAR(100),
+                        company_open_vacancies INTEGER,
+                        company_url VARCHAR(100),
+                        company_description TEXT
+                        )
+                        """)
+        conn.commit()
+        conn.close()
+
+    def create_table_vacancies(self, name_new_database='hh_base',
+                               table_name='vacancies'):
+        """ Создает таблицу вакансии. """
+        conn = psycopg2.connect(dbname=name_new_database, **self.conn_params)
+        conn.autocommit = True
+        with conn.cursor() as cur:
+            cur.execute(f"""
+                        CREATE TABLE IF NOT EXISTS {table_name} (
+                        vacancy_id VARCHAR(20) PRIMARY KEY,
+                        company_id VARCHAR(20),
+                        vacancy_name VARCHAR(100),
+                        vacancy_city VARCHAR(100),
+                        salary_from INTEGER,
+                        salary_to INTEGER,
+                        currency VARCHAR(10),
+                        published TIMESTAMP,
+                        vacancy_url VARCHAR(100),
+                        snippet TEXT,
+                        schedule VARCHAR(50),
+                        experience VARCHAR(50),
+                        employment VARCHAR(50),
+                        company_description TEXT
+                        )
+                        """)
         conn.commit()
         conn.close()
